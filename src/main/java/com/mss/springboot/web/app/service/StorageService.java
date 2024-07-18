111e4944c1939ca7267cb71be20e4c27d0e1a004
mssj-11 <michael11.salgado10@gmail.com>
2024-07-18 17:38:47 -0600

    Service

---
+++ src/main/java/com/mss/springboot/web/app/service/StorageService.java
@@ -0,0 +1,15 @@
+package com.mss.springboot.web.app.service;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+import java.nio.file.Path;
+
+public interface StorageService {
+	
+    void init();
+    String storage(MultipartFile file);
+    Path load(String filename);
+    Resource loadAsResource(String filename);
+    void delete(String filename);
+    
+}
